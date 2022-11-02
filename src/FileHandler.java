0a63f2f9c9f67e7b867d8f6e7349954057088f85
najamoe <najamoe@outlook.dk>
2022-11-02 12:39:56 +0100

    1st - inserted todo

---
+++ src/FileHandler.java
@@ -0,0 +1,15 @@
+public class FileHandler {
+
+    //add method "loadData" that reads csv file from data and prints to screen
+
+
+    //Ret din prototype-kode til, så hver linje bliver læst ind, og “parset” til et Covid19Data objekt.
+    //
+    //I stedet for bare at udskrive hver linje fra csv-filen, skal du lave en anden metode der modtager
+    // linjen (som en samlet String) splitter den op i flere strenge, adskilt af
+    // hvert semikolon, og så parser de strenge til hver sin
+    // attribut (for eksempel skal du bruge Integer.parseInt til at parse en string til en int-attribut).
+    //
+    //Lad loadData metoden oprette en ArrayList af Covid19Data objekter ud fra filens linjer
+    // – husk at ignorere den første linje der indeholder overskrifterne.
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Opret en scanner der læser fra et File-objekt, og lav et loop der læser nextLine fra den scanner, så længe den hasNextLine()
+        // – skriv blot hver linje ud til System.out.
+        //
+        //Kør programmet, og tjek at filen kan læses – der bliver sandsynligvis behov for at tilføje endnu en parameter
+        // til scanneren med den ønskede encoding. Brug StandardCharsets.ISO_8859_1 konstanten.
+        //
+        //Først når du har verificeret at filen kan læses ind, kan du begynde at kode den rigtige applikation.
+
+
+        //Til sidst : Tilføj noget UserInterface der udskriver listen af indlæste Covid19Data objekter.
+    }
+}
